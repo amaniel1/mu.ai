84a980de2567ecea8a4e019cd927df7fbf4de270
amaniel1 <amanialnk1@gmail.com>
2025-12-05 08:28:28 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBh191MKqgfGs1UPJFEuVgW_C7VoXIKId4",
+  authDomain: "mekelle-university-ai.firebaseapp.com",
+  projectId: "mekelle-university-ai",
+  storageBucket: "mekelle-university-ai.firebasestorage.app",
+  messagingSenderId: "719481036353",
+  appId: "1:719481036353:web:02d48a981b0eb05098b0c0",
+  measurementId: "G-15KZL25K72"
 };
 
 export const app = initializeApp(firebaseConfig);
